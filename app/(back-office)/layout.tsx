14ac65bf977b945a9b126b77cc81c7bb44f70431
Faiz Agil Gustianno <faizagil.g@gmail.com>
2025-03-30 19:07:38 +0700

    update layout backoffice buat sidebar

--- app/(back-office)/layout.tsx
+++ app/(back-office)/layout.tsx
@@ -1,20 +1,22 @@
+'use client'
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
-    <div className='flex h-screen'>
-      <Sidebar />
+    <div className='flex h-screen '>
+      <Sidebar isCollapsed={isCollapsed} onToggle={() => setIsCollapsed(prev => !prev)} />
       <div className='flex flex-col w-full'>
         <Header />
-        <main className='flex-1 overflow-y-auto bg-slate-100'>
+        <div
+          className='flex-1 overflow-auto bg-white transition-all duration-300'
+          style={{ marginLeft: isCollapsed ? '80px' : '256px' }}
+        >
           {children}
-        </main>
+        </div>
       </div>
     </div>
   );

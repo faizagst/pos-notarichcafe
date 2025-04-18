0b88d59ab290bd22c794e6a7b0638173e173c523
Faiz Agil Gustianno <faizagil.g@gmail.com>
2025-04-18 19:00:00 +0700

    update layout backoffice

--- app/(back-office)/layout.tsx
+++ app/(back-office)/layout.tsx
@@ -1,22 +1,47 @@
-'use client'
+'use client';
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const [permissions, setPermissions] = useState<{
+    backofficePermissions: Record<string, any>;
+    appPermissions: Record<string, boolean>;
+  } | null>(null);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      const res = await fetch('/api/auth/me');
+      if (res.ok) {
+        const data = await res.json();
+        setPermissions({
+          backofficePermissions: data.user?.backofficePermissions || {},
+          appPermissions: data.user?.appPermissions || {},
+        });
+      } else {
+        console.error('Failed to fetch permissions');
+      }
+    };
+    fetchRole();
+  }, []);
+
+  if (!permissions) return <div>Loading...</div>;
+
+  const sidebarWidth = isCollapsed ? 80 : 256;
 
   return (
-    <div className='flex h-screen '>
-      <Sidebar isCollapsed={isCollapsed} onToggle={() => setIsCollapsed(prev => !prev)} />
-      <div className='flex flex-col w-full'>
+    <div className="h-screen">
+      <Sidebar isCollapsed={isCollapsed} onToggle={() => setIsCollapsed(prev => !prev)} role={permissions} />
+
+      <div
+        className="flex flex-col h-full transition-all duration-300"
+        style={{ paddingLeft: `${sidebarWidth}px` }}
+      >
         <Header />
-        <div
-          className='flex-1 overflow-auto bg-white transition-all duration-300'
-          style={{ marginLeft: isCollapsed ? '80px' : '256px' }}
-        >
+        <main className="flex-1 overflow-auto bg-white">
           {children}
-        </div>
+        </main>
       </div>
     </div>
   );

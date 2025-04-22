269670f3fc6ceb27203b0a3bf198529929fee417
Faiz Agil Gustianno <faizagil.g@gmail.com>
2025-04-22 12:50:26 +0700

    update tampilan saat mengecek permissions

--- app/(app-cashier)/layout.tsx
+++ app/(app-cashier)/layout.tsx
@@ -27,7 +27,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     fetchRole();
   }, []);
 
-  if (!permissions) return <div>Loading...</div>;
+  if (!permissions) {
+    return (
+        <div className="flex items-center justify-center h-64">
+            <div className="flex flex-col items-center">
+                <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+                <p className="mt-4 text-gray-600 text-sm">Memuat data izin akses...</p>
+            </div>
+        </div>
+    );
+}
+
 
   const sidebarWidth = isCollapsed ? 80 : 256;
 
